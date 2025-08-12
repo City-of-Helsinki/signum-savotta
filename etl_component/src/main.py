dffeddae33029433c6ee9ab9b2ddc1a39a43d800
Mikko Vihonen <mikko.vihonen@nitor.com>
2025-08-12 08:10:55 +0300

    ADD misfire grace time to ETL scheduled task

--- etl_component/src/main.py
+++ etl_component/src/main.py
@@ -474,7 +474,7 @@ async def main():
     main
     """
     scheduler = AsyncIOScheduler(job_defaults={"coalesce": True, "max_instances": 1})
-    scheduler.add_job(task, "interval", seconds=30)
+    scheduler.add_job(task, "interval", seconds=30, misfire_grace_time=10)
     scheduler.start()
     try:
         while True:
